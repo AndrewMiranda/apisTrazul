7d7ccbdf8dc9034d52c3e4ca02aaefed253966d7
Juan José Arteta <77931977+jj131204@users.noreply.github.com>
2024-04-14 17:24:45 -0500

    vistas de los emails mas el favicon

---
+++ src/routes/web/web.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+
+router.get("/", (req, res) => {
+    res.send("Bienvenido a TRAZUL");
+});
+
+router.get("/aceptInvitationStaff", (req, res) => {
+    res.render("redirectEmails/aceptInvitationStaff");
+});
+
+
+router.get("/rejectedInvitationStaff", (req, res) => {
+    res.render("redirectEmails/rejectedInvitationStaff");
+});
+
+router.get("/verifyEmail", (req, res) => {
+    res.render("redirectEmails/verifyEmail");
+});
+
+
+module.exports = router;
