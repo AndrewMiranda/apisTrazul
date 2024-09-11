5c1d942035af3fa20ae7518e2e69f4ce8c31ae81
Juan José Arteta <77931977+jj131204@users.noreply.github.com>
2024-09-10 21:57:14 -0500

    home trazul

--- src/public/js/web/home.js
+++ src/public/js/web/home.js
@@ -13,3 +13,38 @@ function hamburgermenufunction(){
         statusMenu = 0;
     }
 }
+
+
+// scrool header
+let lastScrollTop = 0;
+function checkScroll(){
+    let windowHeight = window.innerHeight;
+    let scrollPosition = window.scrollY;
+    let divide = windowHeight / 2;
+
+    let containerOptionHeader = document.getElementsByTagName('header')[0];
+    let optionHeader = document.querySelectorAll('optionHeader');
+
+    if(scrollPosition  >= 1){
+        let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (currentScroll > lastScrollTop) {
+            containerOptionHeader.style.top = "-120px";
+            
+            if(containerOptionHeader.classList.contains("generalShadow")){
+                containerOptionHeader.classList.remove("generalShadow")
+            }
+            
+        }else {
+            containerOptionHeader.style.top = "0px";
+            containerOptionHeader.style.backgroundColor = "#FFFFFF";
+            containerOptionHeader.classList.add("generalShadow");
+            containerOptionHeader.style.position = "fixed";
+            document.querySelectorAll('.optionHeader').forEach(function(element) {
+                element.style.color = '#005fae'; // Cambia el color del texto a rojo
+            });
+        }
+    }
+}
+
+window.addEventListener('scroll', checkScroll, false);
