4d4a7464dcd0a3c0e5347a85773f7f6641b50833
Juan José Arteta <77931977+jj131204@users.noreply.github.com>
2024-09-17 19:42:48 -0500

    seccion trazul

--- src/public/js/web/downloadSection.js
+++ src/public/js/web/downloadSection.js
@@ -1,14 +1,19 @@
-function verifyInputs(){
-    let rolUser = document.getElementsByClassName('rolUser')[0].value;
-    let nameUser = document.getElementsByClassName('nameUser')[0].value;
+function validateDates(){
+    let userName = document.getElementsByClassName('userName')[0].value;
     let userEmail = document.getElementsByClassName('userEmail')[0].value;
     let numberPhone = document.getElementsByClassName('numberPhone')[0].value;
     let message = document.getElementsByClassName('message')[0].value;
 
-    console.log(rolUser, nameUser, userEmail, numberPhone, message);
-    if(rolUser != "" && nameUser != "" && userEmail != "" && numberPhone != ""){
+
+    console.log(userName, userEmail, numberPhone, message);
+    if(userName != "" && userEmail != "" && numberPhone != ""){
         console.log("Todos llenos");
     }else{
         console.log("Faltan campos");
     }
+}
+
+
+function sendForm(){
+    
 }
