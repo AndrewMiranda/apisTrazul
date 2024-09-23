e1b2c29de01de4138ec55d657e1b162a1cd9c7eb
Juan José Arteta <77931977+jj131204@users.noreply.github.com>
2024-09-23 15:40:42 -0500

    cambios de estilo

--- src/public/js/web/home.js
+++ src/public/js/web/home.js
@@ -4,11 +4,18 @@ function verifyInputs(){
     let userEmail = document.getElementsByClassName('userEmail')[0].value;
     let numberPhone = document.getElementsByClassName('numberPhone')[0].value;
     let message = document.getElementsByClassName('message')[0].value;
+    let otherRol = document.getElementsByClassName('otherRol')[0];
 
     console.log(rolUser, nameUser, userEmail, numberPhone, message);
     if(rolUser != "" && nameUser != "" && userEmail != "" && numberPhone != ""){
         console.log("Todos llenos");
     }else{
         console.log("Faltan campos");
+    }
+
+    if(rolUser == "Otro"){
+        otherRol.style.display = "block";   
+    }else{
+        otherRol.style.display = "none";
     }
 }
